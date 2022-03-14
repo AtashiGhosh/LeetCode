a656c0b865b833327ad1c93060787daedf19b924
Atashi Ghosh <47891585+AtashiGhosh@users.noreply.github.com>
2022-03-14 23:20:00 +0600

    Time: 40 ms (24.28%), Space: 13.6 MB (32.05%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        stack = []
+        for elem in path.split("/"):
+            if stack and elem == "..":
+                stack.pop()
+            elif elem not in [".", "", ".."]:
+                stack.append(elem)
+                
+        return "/" + "/".join(stack)
