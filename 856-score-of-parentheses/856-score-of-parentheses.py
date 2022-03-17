357e8c0a6df4ad8ad5cf53648529e27e32ea36cd
Atashi Ghosh <47891585+AtashiGhosh@users.noreply.github.com>
2022-03-18 00:11:39 +0600

    Time: 29 ms (33.33%), Space: 13.4 MB (79.26%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.py
+++ 856-score-of-parentheses/856-score-of-parentheses.py
@@ -4,11 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        stack, cur = [], 0
+        stack, curr = [], 0
         for i in s:
             if i == '(':
-                stack.append(cur)
-                cur = 0
+                stack.append(curr)
+                curr = 0
             else:
-                cur += stack.pop() + max(cur, 1)
-        return cur
+                curr += stack.pop() + max(curr, 1)
+        return curr
